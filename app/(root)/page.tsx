e9f4529090d9b7a04f7e7a9ebfdbaa46bf67f92e
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2025-01-25 18:30:04 +1000

    added the header section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+
 export default function Home() {
   return (
     <div>
-        <h1 className="text-2xl">Home</h1>
+        <section className="pink_container">
+            <h1 className="heading">Discover Powerful AI Agents, Your Vault of Innovation</h1>
+            <p className="sub-heading !max-w-3xl">Search, Learn, and Integrate AI Agents to Unlock New Possibilities.</p>
+        </section>
     </div>
   );
 }
