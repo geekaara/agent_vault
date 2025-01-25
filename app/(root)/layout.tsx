f16d00284038944616036d4c73eb3c60f9b39116
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2025-01-25 21:08:46 +1000

    added the hero section with search bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
-import Navbar from "@/app/components/Navbar";
+import Navbar from "@/components/Navbar";
 
-export default function Layout({children}: Readonly<{children: React.ReactNode}>){
-    return (
-        <main className="font-mono">
-            <Navbar/>
-            {children}
-        </main>
-    )
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+
+export default async function Home({searchParams}: {
+    searchParams: Promise<{ query?: string}>
+}) {
+    const query = (await searchParams).query;
   return (
     <div>
         <section className="pink_container">
             <h1 className="heading">Discover Powerful AI Agents, Your Vault of Innovation</h1>
             <p className="sub-heading !max-w-3xl">Search, Learn, and Integrate AI Agents to Unlock New Possibilities.</p>
+            <SearchForm query={query} />
         </section>
     </div>
   );
